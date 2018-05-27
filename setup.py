eea312ec4f87b20c9001d76dcbddb53dd3aaa87a
Student226123 <Kacper2345!>
2018-05-27 17:59:15 +0200

    dist

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(name='lab11dpp',
 	  version='1.0',
 	  author='Kacper Pabian',
-	  author_email='kapabian226123@e-science.pl',
+	  author_email='226123@student.pwr.edu.pl',
 	  url='git@git.e-science.pl:kapabian226123/lab6dpp.git',
-	  download_url='https://git.e-science.pl/kapabian226123/lab11Libraries/blob/master/lab11dpp-1.0.tar.gz',
+	  download_url='https://github.com/kacperpabian/dpplab11code.git',
 	  packages=['lab6', 'lab6.classes', 'lab6.MainDir'], requires=['PyRECONSTRUCT']
 	  )
